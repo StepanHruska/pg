70200af1a6c64e7425403d841daca91e832c5258
StepanHruska <stepan.hruskacz@gmail.com>
2024-11-14 13:33:55 +0000

    cviceni6

---
+++ cviceni62.py
@@ -0,0 +1,35 @@
+import json
+import requests
+
+
+url = "https://db.carnewschina.com/suggest?q="
+
+
+
+def download_json_and_parse_brands(prefix):
+    # stahneme url + prefix
+    response = requests.get(url + prefix)
+
+    if response.status_code != 200:
+        print("chyba")
+        return []
+
+    data = json.loads(response.content)
+
+    result = []
+
+    brands = data["brands"]
+    for brand in brands:
+        result.append(brand["name"])
+
+    return result
+
+
+if __name__ == "__main__":
+
+    prefix = input("Zadej prefix: ")
+    brands = download_json_and_parse_brands(prefix)
+    for brand in brands:
+        print(brand)
+
+    # pro prefix "ni" mi to vypise Nissan a Nio
